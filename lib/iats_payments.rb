df799b7e01befa37bf027d7d67e3f73b185b1eed
Tim Glen <tim@tag.ca>
2015-06-09 16:36:11 -0400

    require all the files

--- lib/iats_payments.rb
+++ lib/iats_payments.rb
@@ -1,5 +1,9 @@
-require "iats_payments/version"
+require 'savon'
+require 'iats_payments/client'
+require 'iats_payments/client/customer_link_client'
+require 'iats_payments/client/process_link_client'
+require 'iats_payments/client/report_link_client'
+require 'iats_payments/version'
 
 module IatsPayments
-  # Your code goes here...
 end
